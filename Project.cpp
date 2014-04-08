9974e239e3bcc77c3624abb9353a7656f441a703
Steven Huang <ste920ven@gmail.com>
2014-04-08 17:23:56 -0400

    added Project.cpp

---
+++ Project.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc,char *argv[]){
+int unique=0; //count of total unique words
+int total=0; //total number of words
+int stop=0; //number of stop words
+string title; //title name
+
+//data sturcture
+//put stop.txt in a dict
+
+//strip punctuation
+
+//loop through file and build data structure
+
+//output top 5% to report.txt
+
+//print out alphabetically
+}
